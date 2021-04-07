2082032dc41c09c4fb5cf31eba4a414b76c89237
Matias Arrech <matias.arrech@gba.gob.ar>
2021-04-07 18:30:49 -0300

    Added example of routine

--- app/javascript/components/admin/patients/ActiveRoutinesTab.vue
+++ app/javascript/components/admin/patients/ActiveRoutinesTab.vue
@@ -1,20 +1,41 @@
 <template>
   <fragment>
-    <p>Rutinas activas</p>
-    <VueHorizontal responsive>
-      <section
-        v-for="exercice in patient
-          .routines()
-          .target()
-          .first()
-          .exercises()
-          .target().__collection"
-        :key="exercice.id"
-      >
-        <h3>{{ exercice.name }}</h3>
-        <p>{{ exercice.name }}</p>
-      </section>
-    </VueHorizontal>
+    <v-card
+      v-for="routine in patient.routines().target().__collection"
+      :key="routine.id"
+      elevation="2"
+      class="ma-4 pa-4"
+    >
+      <v-card-title>Rutina #{{ routine.id }}</v-card-title>
+      <v-card-subtitle>Creada el {{ routine.created_at }} </v-card-subtitle>
+
+      <v-card-text>
+        <VueHorizontal responsive class="ma-4">
+          <v-card
+            v-for="exercise in routine.exercises().target().__collection"
+            :key="exercise.id"
+            elevation="2"
+            class="ma-4"
+          >
+            <v-card-title>{{ exercise.name }}</v-card-title>
+            <v-card-subtitle>Repeticiones {{ 1 }}</v-card-subtitle>
+          </v-card>
+        </VueHorizontal>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary">
+          Ver
+        </v-btn>
+
+        <v-btn color="primary">
+          Finalizar
+        </v-btn>
+
+        <v-btn>
+          Borrar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </fragment>
 </template>
 
