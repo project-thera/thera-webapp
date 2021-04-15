ab5342d482a087a052af34119018deec5f7a1570
Matias Arrech <matias.arrech@gba.gob.ar>
2021-04-15 15:59:43 -0300

    Refactoring

---
+++ app/javascript/components/admin/patients/RoutineCardActions.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-card-actions>
+    <v-btn
+      v-if="$can('show', routine)"
+      icon
+      :title="$t('views.actions.show')"
+      @click.stop="
+        $router.push({
+          name: 'admin-routines-show',
+          params: { id: routine.id }
+        })
+      "
+    >
+      <v-icon big>{{ $vuetify.icons.values.eye }}</v-icon>
+    </v-btn>
+
+    <ResourceActionButton
+      v-if="$can('discard', routine)"
+      :resource="routine"
+      :title="$t('views.actions.archive')"
+      :message="
+        $t('views.routines.discard.notice', {
+          routine: routine.toString()
+        })
+      "
+      action="discard"
+    >
+      <v-icon medium>{{ $vuetify.icons.values.archiveArrowDown }}</v-icon>
+    </ResourceActionButton>
+
+    <ResourceActionButton
+      v-if="$can('undiscard', routine)"
+      :resource="routine"
+      :title="$t('views.actions.unarchive')"
+      :message="
+        $t('views.routines.undiscard.notice', {
+          routine: routine.toString()
+        })
+      "
+      action="undiscard"
+    >
+      <v-icon medium>{{ $vuetify.icons.values.archiveArrowUp }}</v-icon>
+    </ResourceActionButton>
+
+    <ResourceActionButton
+      v-if="$can('destroy', routine)"
+      :resource="routine"
+      :title="$t('views.actions.destroy')"
+      :message="
+        $t('views.routines.destroy.notice', {
+          routine: routine.toString()
+        })
+      "
+      action="destroy"
+    >
+      <v-icon medium>{{ $vuetify.icons.values.deleteForever }}</v-icon>
+    </ResourceActionButton>
+  </v-card-actions>
+</template>
+
+<script>
+import Routine from "@/resources/Routine";
+
+export default {
+  props: {
+    routine: {
+      type: Routine,
+      required: true
+    }
+  }
+};
+</script>
--- app/javascript/components/admin/patients/RoutinesCards.vue
+++ app/javascript/components/admin/patients/RoutinesCards.vue
@@ -38,69 +38,7 @@
                   </v-list-item-content>
                 </v-list-item>
               </v-list>
-              <v-card-actions>
-                <v-btn
-                  v-if="$can('show', routine)"
-                  icon
-                  :title="$t('views.actions.show')"
-                  @click.stop="
-                    $router.push({
-                      name: 'admin-routines-show',
-                      params: { id: routine.id }
-                    })
-                  "
-                >
-                  <v-icon big>{{ $vuetify.icons.values.eye }}</v-icon>
-                </v-btn>
-
-                <ResourceActionButton
-                  v-if="$can('discard', routine)"
-                  :resource="routine"
-                  :title="$t('views.actions.archive')"
-                  :message="
-                    $t('views.routines.discard.notice', {
-                      routine: routine.toString()
-                    })
-                  "
-                  action="discard"
-                >
-                  <v-icon medium>{{
-                    $vuetify.icons.values.archiveArrowDown
-                  }}</v-icon>
-                </ResourceActionButton>
-
-                <ResourceActionButton
-                  v-if="$can('undiscard', routine)"
-                  :resource="routine"
-                  :title="$t('views.actions.unarchive')"
-                  :message="
-                    $t('views.routines.undiscard.notice', {
-                      routine: routine.toString()
-                    })
-                  "
-                  action="undiscard"
-                >
-                  <v-icon medium>{{
-                    $vuetify.icons.values.archiveArrowUp
-                  }}</v-icon>
-                </ResourceActionButton>
-
-                <ResourceActionButton
-                  v-if="$can('destroy', routine)"
-                  :resource="routine"
-                  :title="$t('views.actions.destroy')"
-                  :message="
-                    $t('views.routines.destroy.notice', {
-                      routine: routine.toString()
-                    })
-                  "
-                  action="destroy"
-                >
-                  <v-icon medium>{{
-                    $vuetify.icons.values.deleteForever
-                  }}</v-icon>
-                </ResourceActionButton>
-              </v-card-actions>
+              <RoutineCardActions :routine="routine" />
             </v-card>
           </v-hover>
         </v-col>
@@ -136,8 +74,12 @@
 
 <script>
 import User from "@/resources/User";
+import RoutineCardActions from "./RoutineCardActions";
 
 export default {
+  components: {
+    RoutineCardActions
+  },
   props: {
     patient: {
       type: User,
