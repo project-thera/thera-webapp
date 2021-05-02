e5180a1a0c26891b8b332b9781968fa6a2af2995
Matias Arrech <matias.arrech@gba.gob.ar>
2021-05-01 21:23:32 -0300

    Added images to seeds

--- db/seeds/4000_speech_exercises.rb
+++ db/seeds/4000_speech_exercises.rb
@@ -1,30 +1,42 @@
 puts "Seeding speech exercises..."
 
-speech_exercise_data = [
-  "perro",
-  "gato",
-  "cocodrilo",
-  "serpiente",
-  "jirafa",
-  "ratón",
-  "murcielago",
-  "pangolín",
-  "auto rojo",
-  "camión verde",
-  "cha cha cha",
-  "campera marrón"
-]
+def read_base64_file(file)
+  return nil unless file
 
-spech_exercises = []
+  # TODO handle mime type
+  File.open(Rails.root.join('db', 'seeds', 'images', file), 'rb') do |img|
+    "data:image/png;base64,#{Base64.strict_encode64(img.read)}"
+  end
+end
 
-speech_exercise_data.each do | sentence |
-  spech_exercises << Exercise.create(
+speech_exercise_data = {
+  'gato' => 'cat.png',
+  'cocodrilo' => 'crocodile.png',
+  'perro' => 'dog.png',
+  'elefante' => 'elephant.png',
+  'raton' => 'mouse.png',
+  'búho' => 'owl.png',
+  'chancho' => 'pig.png',
+  'tortuga' => 'turtle.png',
+  'jirafa' => nil,
+  'murcielago' => nil,
+  'auto rojo' => nil,
+  'camión verde' => nil,
+  'cha cha cha' => nil,
+  'campera marrón' => nil,
+}
+
+speech_exercises = []
+
+speech_exercise_data.each do |sentence, image|
+  speech_exercises << Exercise.create(
     exercise_type: Exercise::EXERCISE_TYPE_SPEECH,
     name: sentence.capitalize,
     description: "Presione el bóton y pronuncie \"#{sentence.capitalize()}\".",
     steps: [
       {
-        goal: sentence
+        goal: sentence,
+        image: read_base64_file(image)
       },
     ].to_json
   )
